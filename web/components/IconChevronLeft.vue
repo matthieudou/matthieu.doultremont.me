17288af0397584f57813407a06ebb43f86e58776
Matthieu d'Oultremont <matthieu.doultremont@gmail.com>
2022-07-24 09:20:02 +0400

    feat(assets): create svg components & png images

---
+++ web/components/IconChevronLeft.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24">
+    <path
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="m15 6l-6 6l6 6" />
+  </svg>
+</template>

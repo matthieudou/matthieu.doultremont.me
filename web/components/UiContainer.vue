aa59d823eefe35cc9aca29faeb645c1ca04a343d
Matthieu d'Oultremont <matthieu.doultremont@gmail.com>
2022-07-24 09:21:44 +0400

    feat(ui): create components

---
+++ web/components/UiContainer.vue
@@ -0,0 +1,13 @@
+<template>
+  <Component
+    :is="as || 'div'"
+    class="px-4 md:px-8 max-w-screen-xl mx-auto">
+    <slot />
+  </Component>
+</template>
+
+<script setup lang="ts">
+  defineProps<{
+    as?: string
+  }>()
+</script>
---
+++ web/components/UiTitle.vue
@@ -0,0 +1,19 @@
+<template>
+  <Component
+    :is="as ?? 'h2'"
+    :class="titleClasses">
+    <slot />
+  </Component>
+</template>
+
+<script setup lang="ts">
+  const props = defineProps<{
+    as?: string
+    level?: 1|2
+  }>()
+
+  const titleClasses = {
+    1: 'text-4xl sm:text-5xl md:text-6xl font-bold leading-tight tracking-wide',
+    2: 'text-xl sm:text-3xl font-semibold tracking-wide'
+  }[props.level ?? 1]
+</script>

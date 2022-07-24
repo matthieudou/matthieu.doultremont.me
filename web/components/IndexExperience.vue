1d600aea9547388ffcde806572e33007da089ded
Matthieu d'Oultremont <matthieu.doultremont@gmail.com>
2022-07-24 19:03:58 +0400

    feat(index): refactor experience

--- web/components/IndexExperience.vue
+++ web/components/IndexExperience.vue
@@ -1,72 +1,57 @@
 <template>
-  <section class="bg-black text-white">
-    <UiContainer class="py-16 md:py-32 grid grid-cols-1 lg:grid-cols-2 gap-16 md:gap-32">
-      <div>
-        <UiTitle>
-          Skills <span class="text-red-500">&</span><br>Experience
-        </UiTitle>
-        <UiTitle
-          class="mt-16 md:mt-32"
-          :level="2">
-          Skills
-        </UiTitle>
-        <ul class="mt-8 md:mt-12 grid grid-cols-2 md:grid-cols-3 gap-8 md:gap-12">
-          <li
-            v-for="(skillItem, skillItemIndex) in SKILL_ITEMS"
-            :key="skillItemIndex">
-            <div class="w-14 h-14 rounded-lg bg-white shadow flex items-center justify-center">
-              <Component
-                :is="skillItem.icon"
-                class="text-black w-8 h-8" />
-            </div>
-            <div class="mt-4">
-              {{ skillItem.text }}
-            </div>
-          </li>
-        </ul>
-      </div>
-      <div>
-        <UiTitle :level="2">
-          Experience
-        </UiTitle>
+  <UiContainer
+    as="section"
+    size="lg"
+    class="py-16 md:py-32">
+    <UiTitle class="md:text-center">
+      Experience
+    </UiTitle>
 
-        <ul class="mt-8 md:mt-12">
-          <li
-            v-for="(experienceItem, experienceItemIndex) in EXPERIENCE_ITEMS"
-            :key="experienceItemIndex"
-            class="border-t border-white grid grid-cols-1 md:grid-cols-4 gap-2 md:gap-4 pt-4 pb-8 items-baseline">
-            <div class="text-sm">
+    <div class="mt-16 md:mt-20 grid grid-cols-[min-content_1fr] md:grid-cols-[1fr_min-content_1fr] gap-x-16 gap-y-0.5 items-stretch">
+      <div
+        v-for="(experienceItem, experienceItemIndex) in EXPERIENCE_ITEMS"
+        :key="experienceItemIndex"
+        class="contents">
+        <div class="hidden md:block text-right">
+          <UiTitle
+            :level="2"
+            as="h3">
+            {{ experienceItem.company }}
+          </UiTitle>
+          <p class="mt-4 tabular-nums">
+            {{ experienceItem.year }}
+          </p>
+        </div>
+        <div class="flex">
+          <div class="-z-10 translate-x-4 h-full border-l border-dashed border-white" />
+          <div class="bg-black rounded-full w-8 h-8 flex items-center justify-center border border-white border-dashed">
+            <div class="w-4 h-4 bg-red-500 rounded-full" />
+          </div>
+        </div>
+        <div class="pb-16">
+          <UiTitle
+            :level="2"
+            as="h3">
+            {{ experienceItem.title }}
+          </UiTitle>
+          <div class="mt-2 flex items-center gap-2 md:hidden">
+            <p class="font-semibold">
+              {{ experienceItem.company }}
+            </p>
+            <p>
               {{ experienceItem.year }}
-            </div>
-
-            <div class="col-span-3">
-              <div class="text-lg font-semibold">
-                {{ experienceItem.title }}
-                <span class="text-sm font-normal">@{{ experienceItem.company }}</span>
-              </div>
-              <div class="mt-1">
-                {{ experienceItem.description }}
-              </div>
-            </div>
-          </li>
-        </ul>
+            </p>
+          </div>
+          <p class="mt-4">
+            {{ experienceItem.description }}
+          </p>
+        </div>
       </div>
-    </UiContainer>
-  </section>
+    </div>
+  </UiContainer>
 </template>
 
 <script setup lang="ts">
-  import IconArrowDownCircle from '~/components/IconArrowDownCircle.vue'
-
-  const SKILL_ITEMS = [
-    { icon: IconArrowDownCircle, text: 'Animation' },
-    { icon: IconArrowDownCircle, text: 'Clean code' },
-    { icon: IconArrowDownCircle, text: 'Design curious' },
-    { icon: IconArrowDownCircle, text: 'Clean components' },
-    { icon: IconArrowDownCircle, text: 'Animation' },
-    { icon: IconArrowDownCircle, text: 'Animation' }
-  ]
-
   const EXPERIENCE_ITEMS = [
     {
       year: '2019 - Present',
